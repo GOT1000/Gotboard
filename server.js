3e28804e933943562cc7cc9c5fdd10d71145ca84
GOT1000 <jinhyg2103@gmail.com>
2017-08-12 22:46:18 +0900

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,9 +10,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+mongoose.connect(process.env.MONGODB_URI);
+
+var Books = require('.models/books');
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/books',function(req,res){
+	Books.find(function(err,books){
+		if(err) return res.status(500).send({error:"An Error has occured"});
+		res.json(books);
+	})
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
